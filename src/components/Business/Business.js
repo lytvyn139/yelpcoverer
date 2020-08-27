7e4984e10ae070b2b2731af4ac6165fa6e48b1cd
IURII <lytvyn.139@gmail.com>
2020-08-26 22:01:27 -0500

    components added

---
+++ src/components/Business/Business.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Business.css';
+
+class Business extends React.Component {
+  render() {
+    return (
+      <div className="Business">
+        <div className="image-container">
+          <img src={this.props.biz.imageSrc} alt=''/>
+        </div>
+        <h2>{this.props.biz.name}</h2>
+        <div className="Business-information">
+          <div className="Business-address">
+            <p>{this.props.biz.address}</p>
+            <p>{this.props.biz.city}</p>
+            <p>{`${this.props.biz.state} ${this.props.biz.zipCode}`}</p>
+          </div>
+          <div className="Business-reviews">
+            <h3>{this.props.biz.category.toUpperCase()}</h3>
+            <h3 className="rating">{`${this.props.biz.rating} stars`}</h3>
+            <p>{`${this.props.biz.reviewCount} reviews`}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Business;
